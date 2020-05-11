868b4d159c7e66b2c88d691baa360e66df847aae
Idan Lavi <idaneurosc@gmail.com>
2020-05-12 01:18:35 +0300

    added doc to class methods added data for model training

--- MSClassifier.py
+++ MSClassifier.py
@@ -11,8 +11,17 @@ f1, df1 = np.vectorize(leaky_relu), np.vectorize(dleaky_relu)  # acts on z1
 f2, df2 = np.vectorize(sigmoid), np.vectorize(dsigmoid)  # acts on z2
 
 
-class CancerClassifier(object):
-    def __init__(self, x_trn, y_trn, x_val, y_val):
+class MSClassifier:
+    __slots__ = ['STEP_SIZE', 'trn_data', 'val_data', 'w1', 'b1', 'w2', 'b2']
+
+    def __init__(self, x_trn: np.ndarray, y_trn: np.ndarray, x_val: np.ndarray, y_val: np.ndarray):
+        """
+        initializing an instance
+        :param x_trn: training data
+        :param y_trn: training labels
+        :param x_val: validation data
+        :param y_val: validation labels
+        """
         self.STEP_SIZE = STEP_SIZE
 
         self.trn_data = x_trn, y_trn
@@ -25,11 +34,14 @@ class CancerClassifier(object):
         self.b2 = np.random.randn(1, 1)
 
     def epoch(self):
+        """
+        iterates over all training data once
+        :return: loss, accuracy
+        """
         x_trn, y_trn = randomize(*self.trn_data)  # randomizing data order - <!><!><!>crucial for convergance<!><!><!>
         n_batch = int(y_trn.shape[0] / mini_batch_size)
         loss, acc = [], []
 
-        # iterate over all training data
         for i in range(n_batch):
             start, end = i * mini_batch_size, (i + 1) * mini_batch_size
             l, a = self.train_batch(x_trn[:, start:end], y_trn[start:end])
@@ -39,10 +51,12 @@ class CancerClassifier(object):
         print(f'epoch loss: {loss:.5f}, epoch accuracy: {acc:.5f}')
         return loss, acc
 
-    def train_batch(self, x, y):
+    def train_batch(self, x: np.ndarray, y: np.ndarray):
         """
-            x shape =  [1024, mini_batch_size]
-            y shape =  [1, mini_batch_size]
+        trains a mini batch of data
+        :param x: array with shape [1024, mini_batch_size]
+        :param y: array with shape [1, mini_batch_size]
+        :return: loss, accuracy
         """
         # layer 1 - input
         z1 = np.dot(self.w1, x) + self.b1
@@ -59,7 +73,18 @@ class CancerClassifier(object):
         acc = 1 - np.sum(abs(y - np.vectorize(thresh)(a2))) / mini_batch_size
         return loss, acc
 
-    def backpropagate(self, x, y, z1, a1, z2, a2):
+    def backpropagate(self, x: np.ndarray, y: np.ndarray,
+                      z1: np.ndarray, a1: np.ndarray,
+                      z2: np.ndarray, a2: np.ndarray):
+        """
+        computes derivatives for weight update
+        :param x: input data
+        :param y: input labels
+        :param z1: w1*x+b1
+        :param a1: f(z1)
+        :param z2: w2*a1+b2
+        :param a2: f(z2)
+        """
         # general gradients (a, z)
         dc_da2 = dfloss(y, a2)  # dC/da2
         da2_dz2 = df2(z2)  # da2/dz2
@@ -86,36 +111,41 @@ class CancerClassifier(object):
         # actual descent
         self.update(dw1, db1, dw2, db2)
 
-    def update(self, dw1, db1, dw2, db2):
+    def update(self, dw1: np.ndarray, db1: np.ndarray, dw2: np.ndarray, db2: np.ndarray):
+        """
+        this actually does the update
+        :param dw1: change in loss /w respect to w1
+        :param db1: change in loss /w respect to b1
+        :param dw2: change in loss /w respect to w2
+        :param db2: change in loss /w respect to b2
+        """
         self.w1 += dw1 * self.STEP_SIZE
         self.w2 += dw2 * self.STEP_SIZE
         self.b1 += db1 * self.STEP_SIZE
         self.b2 += db2 * self.STEP_SIZE
 
     def validate(self):
+        """
+        assesses the models accuracy and loss on validation data
+        :return: loss, accuracy
+        """
         x, y = self.val_data
-
-        # layer 1
-        a1 = f1(np.dot(self.w1, x) + self.b1)
-
-        # layer 2
-        a2 = f2(np.dot(self.w2, a1) + self.b2)
-
+        a1 = f1(np.dot(self.w1, x) + self.b1)  # layer 1
+        a2 = f2(np.dot(self.w2, a1) + self.b2)  # layer 2
         # computing loss and accuracy
         loss = np.mean(floss(a2, y))
         acc = np.mean(1 - abs(y - np.vectorize(round)(a2)))
 
-        # loss-sensetive step size
-        # self.step_correct(loss)    ->    it actually works better with no step correct
-
         print(f'total val loss: {loss:.5f}, total val accuracy: {acc:.5f}\n')
         return loss, acc
 
-    def step_correct(self, loss):
-        if loss < 0.15: self.STEP_SIZE = 5e-4
-        if loss < 0.075: self.STEP_SIZE = 2.5e-4
-
-    def to_json(self, va, filename=None):
+    def to_json(self, va: float, filename: str = None):
+        """
+        saves a dictionary with relevant values
+        :param va: validation accuracy
+        :param filename: save to a file named {filename}
+        :return:
+        """
         # this function saves a json version of the dict in to a file
         import json
         import datetime as dt
@@ -131,8 +161,14 @@ class CancerClassifier(object):
             json.dump(trained_dict, f)
 
 
-def plotting(tl, ta, vl, va):
-    # this plots the loss and accuracy
+def plotting(tl: list, ta: list, vl: list, va: list):
+    """
+    plots the training and validation loss and accuracy along training
+    :param tl: training loss
+    :param ta: training accuracy
+    :param vl: validation loss
+    :param va: validation accuracy
+    """
     plt.figure(), plt.subplot(211)
     plt.plot(tl, label='train'), plt.plot(vl, label='validation')
     plt.title("loss"), plt.legend(), plt.xlabel("epochs"), plt.ylabel("precents")
@@ -142,9 +178,18 @@ def plotting(tl, ta, vl, va):
     plt.show()
 
 
-def run_epochs(epochs, trn_loss, trn_acc, val_loss, val_acc):
-    for e in range(epochs):
-        print(f"run number: {e + 1}/{epochs}")
+def run_epochs(ep: int, trn_loss: list, trn_acc: list, val_loss: list, val_acc: list):
+    """
+    training on all the training data {epochs} times
+    :param ep: number of training sessions on all of the data
+    :param trn_loss: training loss
+    :param trn_acc: training accuracy
+    :param val_loss: validation loss
+    :param val_acc: validation accuracy
+    :return: input args without epochs
+    """
+    for i in range(ep):
+        print(f"run number: {i + 1}/{ep}")
         tl, ta = cc.epoch()
         vl, va = cc.validate()
         trn_loss.append(tl), trn_acc.append(ta), val_loss.append(vl), val_acc.append(va)
@@ -152,10 +197,9 @@ def run_epochs(epochs, trn_loss, trn_acc, val_loss, val_acc):
 
 
 if __name__ == "__main__":
-
-    cc = CancerClassifier(*data_load(os.getcwd()))
-    trn_loss, trn_acc, val_loss, val_acc = [], [], [], []
-    trn_loss, trn_acc, val_loss, val_acc = run_epochs(epochs, trn_loss, trn_acc, val_loss, val_acc)
+    cc = MSClassifier(*data_load(os.getcwd()))
+    loss_acc = [], [], [], []
+    loss_acc = run_epochs(epochs, *loss_acc)
     # trn_loss, trn_acc, val_loss, val_acc = run_epochs(100, trn_loss, trn_acc, val_loss, val_acc)
     # cc.to_json(val_acc[-1], filename='fn')
-    plotting(trn_loss, trn_acc, val_loss, val_acc)
+    plotting(*loss_acc)
